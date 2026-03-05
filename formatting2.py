234e7b2cac14fa27ce3889c01bf33808a45e62e3
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:23:22 -0500

    Create formatting2.py

---
+++ formatting2.py
@@ -0,0 +1,5 @@
+#This program demonstrates how a floating-point 
+# number can be formatted.
+amount-due = 5000.0
+monthly-apyment = amount-due / 12.0
+print('The monthly payment is %.2f' % monthlypayment)
