994897349dab4c64fc83354fae55c8e7215ea31b
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:20:24 -0500

    Create no_formatting.py

---
+++ no_formatting.py
@@ -0,0 +1,5 @@
+#This program demonstrates how a floating-point 
+#number is displayed with no formatting.
+amount-due = 5000.0
+monthly-payment = amount-due / 12.0
+print('The monthly payment is', monthly-payment)
