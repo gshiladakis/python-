800c2335caa20c8fd1e996e8f9d9e4a4e5b7c1d5
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:27:31 -0500

    Create decimal_places.py

---
+++ decimal_places.py
@@ -0,0 +1,10 @@
+#This program demonstrates how a value can be 
+#formatted, rounded to different numbers of
+# decimal places.
+my-value = 1.123456789
+print '%.lf' % my-value # Rounded to 1 decimal place 
+print '%.2f1 % my-value # Rounded to 2 decimal places 
+print '%.3f1 % my-value # Rounded to 3 decimal places 
+print ' %. 4f ' % my-value # Rounded to 4 decimal places 
+print ' %. 5f ' % my-value # Rounded to 5 decimal places 
+print ' %. 6f' % my-value # Rounded to 6 decimal places 
