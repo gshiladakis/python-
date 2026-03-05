67d25b53c8818e48c6692f9c25806d34a1f5324b
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:44:36 -0500

    Create names_and_sales.py

---
+++ names_and_sales.py
@@ -0,0 +1,15 @@
+# This program displays a set of salesperson
+# names and units sold in two columns.
+
+# Assign the names to variables.
+salesperson1 = 'Graves'
+salesperson2 = 'Harrison'
+salesperson3 = 'Hoyle'
+salesperson4 = 'Kramer'
+salesperson5 = 'Smith'
+
+# Assign the units sold to variables.
+units1 = 1456.78
+units2 = 2890.55
+units3 = 946.77
+units4 = 2678.91
