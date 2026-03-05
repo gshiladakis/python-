9b8992aa72b53b2231ee424ada0a9968949aca93
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:53:06 -0500

    Update names_and_sales.py

--- names_and_sales.py
+++ names_and_sales.py
@@ -13,3 +13,12 @@ units1 = 1456.78
 units2 = 2890.55
 units3 = 946.77
 units4 = 2678.91
+units5 = 1287.87
+
+# Display the data.
+print('%15s %15s' % ('Salesperson', 'Units Sold'))
+print('%15s %15d' % (salesperson1, units1))
+print('%15s %15d' % (salesperson2, units2))
+print('%15s %15d' % (salesperson3, units3))
+print('%15s %15d' % (salesperson4, units4))
+print('%15s %15d' % (salesperson5, units5))
