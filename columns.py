15ff34f561429510ec26f1c4d6edc7ecce9108bc
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-05 12:33:37 -0500

    Create columns.py

---
+++ columns.py
@@ -0,0 +1,18 @@
+#This program displays the following 
+# floating-point numbers in a column
+# with their decimal points aligned.
+num1 = 127.899
+num2 = 3465.148
+num3 = 3.776
+num4 = 264.821
+num5 = 88.081
+num6 = 799.999
+
+# Display each number in a field of 7 spaces
+# with 2 decimal places.
+print('%7.2f' % num1)
+print('%7.2f' % num2)
+print('%7.2f' % num3)
+print('%7.2f' % num4)
+print('%7.2f' % num5)
+print('%7.2f' % num6)
