b4ddb692b546d0526f767ebd6c29263ad8d3faa6
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-09 09:10:00 -0400

    Update car_demo.py

--- car_demo.py
+++ car_demo.py
@@ -18,3 +18,111 @@ def main():
 # Call the main function.
 if _ _name_ _ == '__main_ _':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# The Truck class represents a pickup truck. It is a 
+# subclass of the Automobile class.
+
+class Truck(Automobile):
+  # The __init__ method accepts arguments for the 
+  # Truck's make, model, mileage, price, and drive type.
+
+  def __init__(self, make, model, mileage, price, drive_type):
+      # Call the superclass's __Init__ method and pass
+      # the required arguments. Note that we also have
+      # to pass self as an argument.
+      Automobile.__init__(self, make, model, mileage, price)
+
+      # Initia;ize the __drive__type attribute. 
+      self.__drive_type = drive_type
+
+   # The set_drive_type method is the mutator for the 
+   # __drive_type attribute.
+
+  def set_drive_type(self, drive_type):
+    self.__drive = drive_type
+
+  # The get_drive_type method is the accessor for the 
+  # __drive_type attribute.
+
+  def get_drive_type(self):
+      return self.__drive_type
+
+# The SUV class represents a sport utility vehicle. It
+# is a subclass of the Automobile class.
+
+class SUV(Automobile):
+    # The __init__ method accepts arguments for the 
+    # SUV's make, model, mileage, price, and passenger
+    # capacity.
+
+    def __init__(self, make, model, mileage, price, pass_cap):
+        # Call the superclass's __init__ method and pass
+        # the required arguments. Note that we also have
+        # to pass self as an argument.
+        Automobile.__init__(self, make, model, mileage, price)
+
+        # Initialize the __pass_cap attribute.
+        self.__pass_cap = pass_cap 
+
+    # The set_pass_cap method is the mutator for the 
+    # __pass_cap attribute.
+
+    # def set_pass_cap(self, pass_cap):
+          self.__pass_cap = pass_cap
+
+    # The get_pass_cap method is the accessor for the 
+    # __pass_cap attrbute.
+
+    def get_pass_cap(self):
+        return self.__pass_cap 
