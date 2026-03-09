8931d662c24ed760990c8788d212ec2b05d851fc
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-09 12:40:03 -0400

    Create books_per_month2.py

---
+++ books_per_month2.py
@@ -0,0 +1,11 @@
+# Get the number of books the user plans to read.
+books = input('How many books do you want to read? ')
+
+# Get the number of months it will take to read them.
+months = input('How many months will it take? ')
+
+# Calcualte the number of books per month.
+per-month = float(books) / months
+
+# Display the result.
+print('You will read', per-month, 'books per month.')
