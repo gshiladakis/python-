1465a86ca5f8ec968c653927936471a050a5117b
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-09 08:47:23 -0400

    Create car_demo.py

---
+++ car_demo.py
@@ -0,0 +1,20 @@
+# This program demonstrates the Car class.
+
+import vehicles
+
+def main():
+    # Create an object from the Car class.
+    # The car is a 2007 Audi with 12,500 miles, priced
+    # at $21,500,00 and has 4 doors.
+    used_car = vehicle.Car('Audi', 2007, 12500, 21500.0, 4)
+
+    # Display the car's data.
+    print('Make:', used_car.get_make())
+    print('Model:', used_car.get_model())
+    print('Mileage:', used_car.get_mileage())
+    print('Price:', used_car.get_price())
+    print('Number of doors:', used_car.get_doors())
+
+# Call the main function.
+if _ _name_ _ == '__main_ _':
+    main()
