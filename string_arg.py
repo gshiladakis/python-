775e7a22c1c780ea6b11551964f88ef2cd485c39
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-12 13:05:29 -0400

    Create string_arg.py

---
+++ string_arg.py
@@ -0,0 +1,14 @@
+#This program demonstrates passing two string
+#arguments to a function
+
+def main():
+  first_name = raw-input('Enter your first name: ')
+  last_name = raw_input('Enter your last name: ')
+  print('Ypur name reversed is')
+  reverse_name(first_name, last_name)
+
+def reserve_name(first, last):
+    print(last, first)
+
+# Call the main function.
+main()
