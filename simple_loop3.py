b6fa141e6d00156449395b53cadee79297444499
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-13 12:45:33 -0400

    Create simple_loop3.py

---
+++ simple_loop3.py
@@ -0,0 +1,9 @@
+# This program alos demonstrates a simple for
+# loop that uses a list of numbers.
+
+def main():
+    for name in ['Winken', 'Blinken', 'Nod']:
+        print(name)
+
+# Call the main function.
+main()
