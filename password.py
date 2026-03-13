76d99f97a36de9420d87b8b1c6d6da02399e5da6
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-13 11:03:24 -0400

    Create password.py

---
+++ password.py
@@ -0,0 +1,16 @@
+# This program demonstrates how the == operator can 
+# be used to compare strings.
+
+def main():
+    # Get a password from the user.
+    password = raw_input('Enter the password: ')
+
+    # Determine whether the correct password
+    # was entered.
+    if password == 'prospero':
+        print('Password accepted.')
+    else:
+        print('Sorry, that is the wrong password.')
+
+# Call the main function.
+main() 
