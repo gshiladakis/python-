84ebf9602cef13568ef4bbb49e07f4350859293c
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-13 17:54:51 -0400

    Create division_Operation.py

---
+++ division_Operation.py
@@ -0,0 +1,9 @@
+def print_div(op_1, op_2):
+  """Prints division operation"""
+  print(f"{op_1}/{op_2} = {op_1/op_2}")
+
+num_1 = 6
+num2 = 3
+print_div(num_1, num_2) # Prints "6/3 = 2.0"
+print_div(num_2, num_1) # Prints "3/6 = 0.5"
+print_div(num_1) # Error: Missing argument: op_2
