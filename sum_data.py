ceaf51c97a4df0239c4f432580696941fc02fd1f
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-17 11:37:35 -0400

    Create sum_data.py

---
+++ sum_data.py
@@ -0,0 +1,17 @@
+data = [1, 5, 6, 3, 12, 3]
+
+sum = 0
+
+# Find the Sum of all the numbers
+for x in data:
+  sum = sum + x
+
+print(x)
+
+# Find the Mean or Average of all numbers
+
+N = len(data)
+
+mean = sum / N
+
+print(Mean)
