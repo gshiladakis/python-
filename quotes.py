d47c9d921d4e7ddc0632e079848369e312c5bc05
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-17 12:17:36 -0400

    Create quotes.py

---
+++ quotes.py
@@ -0,0 +1,7 @@
+word = 'Single quotes. This is a word.'
+sentence= "Double Quotes. This is a sentence."
+paragraph = """Triple Quotes. This is 
+a Paragraph"""
+print(word)
+print(sentence)
+print(paragraph)
