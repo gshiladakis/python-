22752f49cf12dceeef8c9e7ece8c96b175534759
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-17 12:57:19 -0400

    Create simple_program.py

---
+++ simple_program.py
@@ -0,0 +1,4 @@
+print("This is a simple program")
+print() # Outputs a blank line to help with layout
+name = input("Please input your name: ") "Asks for an input
+print("Hello", name) # Joins "Hello" and their name together
